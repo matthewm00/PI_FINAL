05763402d30b9e7859ca3a4a7028ff18d70d2853
Mateo Menghini <mateo.menghini@gmail.com>
2020-12-02 00:07:58 -0300

    2do commit!

--- barriosADT.c
+++ barriosADT.c
@@ -5,15 +5,19 @@
 #include <math.h>
 #include "barriosADT.h"
 
+#define BLOQUE 50
+
 typedef struct barrio
 {
     char *name;
     size_t habitants;
+    size_t treesPerHab;
 } tBarrio;
 
 struct node
 {
     tBarrio barr;
+    char *popularTree; //nombre del arobol + popular del barrio
     struct node *tail;
 };
 typedef struct node *TNodeBarrio;
@@ -22,4 +26,41 @@ struct barrioCDT
 {
     TNodeBarrio first;
     TNodeBarrio current; //para iterar
+    tBarrio *vec;
+    size_t size;
 };
+static int checkMemory()
+{
+    if (errno != ENOMEM)
+        return 1;
+    perror("ERROR");
+    errno = 0;
+    return 0;
+}
+
+barrioADT newBarrio()
+{
+    barrioADT barr = calloc(1, sizeof(struct barrioCDT));
+    if (!checkMemory())
+        return NULL;
+    return barr;
+}
+
+int addTree(barrioADT barr, const char *name, size_t habitants)
+{
+
+    if (barr->size % BLOQUE == 0)
+    {
+        barr->vec = realloc(barr->vec, (barr->size + BLOQUE) * sizeof(tBarrio));
+        if (!checkMemory())
+            return 0;
+    }
+
+    barr->vec[barr->size].name = malloc(strlen(name) + 1);
+    if (!checkMemory())
+        return 0;
+    strcpy(barr->vec[barr->size].name, name);
+    barr->vec[barr->size].habitants = habitants;
+    barr->size++;
+    return 1;
+}
--- treesADT.c
+++ treesADT.c
@@ -5,12 +5,15 @@
 #include <math.h>
 #include "treesADT.h"
 
+#define BLOQUE 50
+
 typedef struct tree
 {
     char *neighbourhood;
     char *street;
     char *name; //nombre cientifico
     size_t diameter;
+    size_t appearences;
 } tTree;
 struct node
 {
@@ -20,8 +23,56 @@ struct node
 };
 typedef struct node *TNodeTree;
 
-struct barrioCDT
+struct treeCDT
 {
     TNodeTree first;
     TNodeTree current; //para iterar
+    tTree *vec;
+    size_t size;
 };
+
+static int checkMemory()
+{
+    if (errno != ENOMEM)
+        return 1;
+    perror("ERROR");
+    errno = 0;
+    return 0;
+}
+
+treeADT newTree()
+{
+    treeADT t = calloc(1, sizeof(struct treeCDT));
+    if (!checkMemory())
+        return NULL;
+    return t;
+}
+
+int addTree(treeADT t, const char *name, const char *neighbourhood)
+{
+    /*for (int i = 0; i < t->size; i++)
+    {
+        if (strcmp(t->vec[i].name, name) == 0)
+        {
+            t->vec[i].appearences++;
+            return 1;
+        }
+    }
+    */
+    if (t->size % BLOQUE == 0)
+    {
+        t->vec = realloc(t->vec, (t->size + BLOQUE) * sizeof(tTree));
+        if (!checkMemory())
+            return 0;
+    }
+
+    t->vec[t->size].name = malloc(strlen(name) + 1);
+    t->vec[t->size].neighbourhood = malloc(strlen(neighbourhood) + 1);
+    if (!checkMemory())
+        return 0;
+    strcpy(t->vec[t->size].name, name);
+    strcpy(t->vec[t->size].neighbourhood, neighbourhood);
+    t->vec[t->size].appearences = 1;
+    t->size++;
+    return 1;
+}
